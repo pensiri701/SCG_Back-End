6e4184b1ec9cd956c834c125f4225a8ed1f3a08d
penguinnoi <pensiri701@gmail.com>
2020-05-17 20:52:06 +0700

    frist commit

---
+++ resources/views/Layout/footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+ <div class="container-fluid">
+
+   <div class="copyright float-right">
+     &copy;
+     <script>
+       document.write(new Date().getFullYear())
+     </script>, made with <i class="material-icons"></i> by
+     <a href="https://www.creative-tim.com" target="_blank">pensiri kusalaporn TEST</a>
+   </div>
+ </div>
+</footer>
