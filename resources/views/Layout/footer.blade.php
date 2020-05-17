a3ca13ca5e75d4823cf74626435f2a9b1d5e9e39
penguinnoi <pensiri701@gmail.com>
2020-05-17 21:33:19 +0700

    footer

--- resources/views/Layout/footer.blade.php
+++ resources/views/Layout/footer.blade.php
@@ -5,8 +5,8 @@
      &copy;
      <script>
        document.write(new Date().getFullYear())
-     </script>, made with <i class="material-icons"></i> by
-     <a href="https://www.creative-tim.com" target="_blank">pensiri kusalaporn TEST</a>
+     </script>, made by
+     <a href="" target="_blank">pensiri kusalaporn TEST</a>
    </div>
  </div>
 </footer>
